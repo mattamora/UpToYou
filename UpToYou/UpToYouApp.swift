ad525620df58759b78bc77d343ab3ba38ab2ae61
Matthew Amora <mattamora@csu.fullerton.edu>
2025-02-28 16:35:38 -0800

    Initial Commit

---
+++ UpToYou/UpToYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpToYouApp.swift
+//  UpToYou
+//
+//  Created by Matthew Amora on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct UpToYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UpToYouTests/UpToYouTests.swift
@@ -0,0 +1,17 @@
+//
+//  UpToYouTests.swift
+//  UpToYouTests
+//
+//  Created by Matthew Amora on 2/28/25.
+//
+
+import Testing
+@testable import UpToYou
+
+struct UpToYouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
