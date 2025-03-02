786d94a9aa1f21794bd9b92e75b54757a3a6120c
Matthew Amora <mattamora@csu.fullerton.edu>
2025-03-02 11:39:51 -0800

    Includes/installs firebase

--- UpToYou/UpToYouApp.swift
+++ UpToYou/UpToYouApp.swift
@@ -3,12 +3,25 @@
 //  UpToYou
 //
 //  Created by Matthew Amora on 2/28/25.
-//
+//  App startup
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct UpToYouApp: App {
+    // register app delegate for Firebase setup
+      @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             Home_Screen()
